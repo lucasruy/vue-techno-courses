d0784b34e01abf4f72726d774e42e0ffef02c36c
lucasruy <lucasruy@sas.com.br>
2022-01-12 12:04:23 -0300

    feat: create Contact component

---
+++ src/views/Contact.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <PageLoading/>
+    </div>
+    <transition>
+      <div v-if="error">
+        <h1>Ops... Houve um problema, volte mais tarde.</h1>
+      </div>
+    </transition>
+    <transition>
+      <div v-if="content">
+        <h1>{{content.title}}</h1>
+        <p>{{content.description}}</p>
+        <ul>
+          <li>
+            <b>Email:</b>
+            {{content.details.email}}
+          </li>
+          <li>
+            <b>Telefone:</b>
+            {{content.details.phone}}
+          </li>
+          <li>
+            <b>Endereço:</b>
+            {{content.details.address}}
+          </li>
+        </ul>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import getPageData from '@/mixins/getPageData.js'
+
+export default {
+  name: 'Contact',
+  mixins: [getPageData],
+  created() {
+    this.getPageData('/contact')
+  }
+}
+</script>
